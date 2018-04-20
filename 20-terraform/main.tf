3e8a1f03236408a8ee901ac08e065e146f595c90
Radek Simko <radek.simko@gmail.com>
2018-04-20 15:18:14 +0200

    Make deployments unique

--- 20-terraform/main.tf
+++ 20-terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-3"
+  region = "${var.aws_region}"
   profile = "tf"
 }
 
---
+++ 20-terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  default = "eu-west-3"
+}
+
+variable "prefix" {
+  default = "radek-devheaven"
+}
+
+variable "s3_bucket" { }
+variable "s3_key" { }
